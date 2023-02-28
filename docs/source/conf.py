b6544b0b5cdbbe505af88a546cc7c0d8077e2801
Takashi OKADA <tk.okada.github@gmail.com>
2023-02-28 16:20:16 +0900

    udpate

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,14 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-
 # -- Project information -----------------------------------------------------
 
-project = 'opt'
-copyright = '2021, AIST'
+project = 'aiaccel'
+copyright = '2022, AIST'
 author = 'AIST'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +29,13 @@ release = '0.0.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx_rtd_theme',
+    'sphinx_fontawesome',
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -39,7 +44,26 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Add optional syntaxes for MyST-Parser which are disabled by default.
+myst_enable_extensions = [
+    # "amsmath",
+    # "attrs_inline",
+    # "colon_fence",
+    # "deflist",
+    "dollarmath",
+    # "fieldlist",
+    # "html_admonition",
+    # "html_image",
+    # "inv_link",
+    # "linkify",
+    # "replacements",
+    # "smartquotes",
+    # "strikethrough",
+    # "substitution",
+    # "tasklist",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,10 +71,19 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
+html_show_sphinx = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+
+gettext_compact = False
+locale_dirs = ['locale/']
+language = 'ja'
+
+# -- Extension configuration -------------------------------------------------
+todo_include_todos = True
+# autoclass_content = "both"
